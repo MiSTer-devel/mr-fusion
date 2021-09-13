0265c593d8963c3b8676366a53f83f020e7566fd
Michael Smith <m@michaelsmith.be>
2021-09-13 14:35:03 +0200

    Speed up build: redirect build output to /dev/null

--- scripts/build.sh
+++ scripts/build.sh
@@ -18,10 +18,10 @@
 # buildroot
 cd ~/buildroot
 make mrfusion_defconfig
-make
+make >&/dev/null
 
 # Linux
 cd ~/linux-socfpga
 make ARCH=arm CROSS_COMPILE=~/buildroot/output/host/bin/arm-buildroot-linux-uclibcgnueabi- mrfusion_defconfig
-make ARCH=arm CROSS_COMPILE=~/buildroot/output/host/bin/arm-buildroot-linux-uclibcgnueabi- -j10
+make ARCH=arm CROSS_COMPILE=~/buildroot/output/host/bin/arm-buildroot-linux-uclibcgnueabi- -j10 >&/dev/null
 make ARCH=arm CROSS_COMPILE=~/buildroot/output/host/bin/arm-buildroot-linux-uclibcgnueabi- socfpga_cyclone5_socdk.dtb
